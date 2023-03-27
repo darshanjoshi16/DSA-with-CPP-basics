c4d94dc7d8f32e9e2c49558c8bb2992e53a4833c
Darshan Joshi <darshanjoshi9664905879@gmail.com>
2023-03-27 12:21:23 +0530

    added the logic for brute force and optimized approach for the checking prime number in PracticeofAll.cpp

--- Basic Maths/PracticeofAll.cpp
+++ Basic Maths/PracticeofAll.cpp
@@ -121,7 +121,7 @@ void isArmStrongNum(int N)
     }
 }
 
-//defining the function for printing the divisiors with brute force approach
+//defining the function for printing the divisiors with brute force approach TC = O(N)
 void PrintAlldivisiorsBrute(int N)
 {
     cout << "Print All Divisors (Brute Force)" << endl;
@@ -135,6 +135,76 @@ void PrintAlldivisiorsBrute(int N)
     }
 }
 
+//defining the function for printing the divisiors with optimized approach TC = O(sqrt(N))
+void PrintAlldivisiorsOptimized(int N)
+{
+    //defining the list for storing the divisiors 
+    vector <int> ls;
+
+    // //here we will use i*i instead of sqrt(N) due to reduction in the time complexity
+    for(int i=1; i*i<=N ; i++)
+    {
+        //checking for pair of factors with divisibility
+        if(N % i == 0)
+        {
+            ls.emplace_back(N);
+            
+            //checking for another factor is same or not with i then we can add into vector
+            if(i != N/i){ls.emplace_back(N/i);}
+        }
+
+        //sorting the vector for ascending manner for values in the vector
+        sort(ls.begin(),ls.end());
+
+        //iterating over vector for displaying the value
+        cout << "The Prime Numbers: ";
+
+        for(auto it:ls){cout << it << " ";}
+
+    }
+}
+//defining the function for checking the prime number using the brute force approach TC=O(N)
+void CheckingPrimeBruteForce(int N)
+{
+    //as per the definition of prime number : it must have exactly 2 factors : 1 and number itself
+    //initialize the variable for storing the count of factors
+    int FactorCount = 0;
+
+    //now running the loop N times which will having TC of O(N)
+    for(int i=1 ; i<= N ; i++)
+    {
+        //checking the prime number condition
+        if(N % i == 0){FactorCount++;}
+    }
+
+    //checking the count for the prime number condition fulfilling
+    if(FactorCount == 2) { cout << "The Given Number is Prime";}
+    else{cout << "Given number is not Prime";}
+
+}
+
+//defining the function for checking the prime number using the optimized approach TC = O(sqrt(N))
+void CheckingPrimeOptimized(int N)
+{
+    //initializing the variable for storing the count of factor
+    int FactorCount = 0;
+
+    //now we will run the loop for the optimized way of TC = O(Sqrt(N))
+    for(int i=1 ; i*i<=N ; i++)
+    {
+        //checking the condition for prime number
+        if(N % i == 0)
+        {
+            FactorCount++;
+            //checking for another part of pair
+            if(i != N/i){FactorCount++;}
+        }
+    }
+
+    //checking the count for the prime number
+    if(FactorCount == 2) { cout << "The Given Number is Prime";}
+    else{cout << "The Given number is not prime";}
+}
 //================================================================================================================================================
 
 //declaring the main function
@@ -149,5 +219,7 @@ int main()
     cin >> N;
     
     //calling the function as per the requirement
+    
+    
     return 0;
 }

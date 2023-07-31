c267de782a9115af57130e4feb7f7344f3410877
darshanjoshi16 <darshanjoshi9664905879@gmail.com>
2023-07-31 10:48:44 +0530

    adding the new file of parameterized recursive approach

---
+++ Recursion/SumofNParameterized.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+//sum of First N number using the parameterized functional apporach in recursion
+void sumofN(int i, int sum,int N)
+{
+    if(i > N)
+    {
+        cout << sum;
+        return;
+    }
+    else
+    {
+        sumofN(i+1,sum+i,N);
+    }
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+    sumofN(0,0,N);
+}
